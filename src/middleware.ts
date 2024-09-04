a4b8a3d5c898b1ade207bde3d89437c8132fa54a
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-09-04 02:52:04 +0100

    setting up clerk auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,42 @@
 import createMiddleware from 'next-intl/middleware';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { locales, localePrefix, defaultLocale } from './config';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
-export default createMiddleware({
+const intlMiddleware = createMiddleware({
   // A list of all locales that are supported
-  locales: ['fr', 'ar'],
+  locales: locales,
 
   // Used when no locale matches
-  defaultLocale: 'fr',
+  defaultLocale: defaultLocale,
+
+  // localePrefix
+  localePrefix: localePrefix,
 });
 
+const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/darja-admin(.*)']);
+
+export default function middleware(req: NextRequest, event: NextFetchEvent) {
+  // Apply Clerk middleware to protected routes
+  if (isProtectedRoute(req)) {
+    return clerkMiddleware((auth, req) => {
+      auth().protect(); // Protect the route
+      return NextResponse.next(); // Proceed with the request
+    })(req, event);
+  }
+
+  // Apply `next-intl` middleware to other routes
+  return intlMiddleware(req);
+}
+
 export const config = {
-  // Match only internationalized pathnames
   matcher: [
-    '/',
-    '/about',
-    '/derive-2024',
-    '/previous',
-    '/espace-darja',
-    '/community',
-    '/contact',
+    // Match paths for `next-intl`
     '/(fr|ar)/:path*',
+    // Match paths for Clerk (admin panel)
+    '/admin(.*)',
+    '/darja-amin(.*)',
+    // Ensure the root path and other paths are included
+    '/',
   ],
 };
