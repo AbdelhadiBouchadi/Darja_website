42487be3f5d2a09b34077a3dcb3c19039ec39614
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-09-10 15:46:56 +0100

    updating font dislay

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -10,7 +10,7 @@ import Header from '../../components/shared/header';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Espace Darja',
+  title: 'Dérive Casablancaise',
   description:
     "Rejoignez-nous à Darja, le festival de musique ultime célébrant la diversité des rythmes et des cultures. Vivez une fusion de sons traditionnels et contemporains, avec des performances d'artistes de renommée mondiale dans une ambiance inoubliable. Découvrez la magie de la musique à Darja !",
 };
