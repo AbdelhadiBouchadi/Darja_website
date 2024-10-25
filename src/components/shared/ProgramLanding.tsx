ff9e5cee0d226a500d7992e11f4ae3d41ef08e8d
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-10-25 12:53:27 +0300

    updating programs page

--- src/components/shared/ProgramLanding.tsx
+++ src/components/shared/ProgramLanding.tsx
@@ -5,6 +5,8 @@ import { useInView, motion, useTransform, useScroll } from 'framer-motion';
 import React, { useEffect, useRef, useState } from 'react';
 import SubHeader from './SubHeader';
 import { useLocale, useTranslations } from 'next-intl';
+import Link from 'next/link';
+import RoundedBtn from './rounded';
 
 const ProgramLanding = () => {
   const sectionRef = useRef(null);
@@ -13,6 +15,7 @@ const ProgramLanding = () => {
   const [isMobile, setIsMobile] = useState(false);
   const locale = useLocale();
   const isArabic = locale === 'ar';
+  const t = useTranslations('Derive2024');
   const t2 = useTranslations('HomePage');
 
   // Detect if the user is on a smaller screen (e.g., mobile)
@@ -49,11 +52,11 @@ const ProgramLanding = () => {
       </motion.main>
       <motion.div
         ref={imageContainerRef}
-        className="bg-derive w-full min-h-screen  relative my-64 xl:my-80 2xl:my-96"
+        className="bg-derive w-full min-h-screen  relative my-64 xl:my-80 2xl:my-96 flex flex-col items-center"
         style={{ backgroundSize }}
       >
         <motion.div
-          className="absolute right-0 top-0 h-[80%] xl:bg-[#E9EAEB]  xl:w-[40%] flex flex-col justify-center items-center gap-4 2xl:gap8 text-[#00b0db] xl:text-black/90"
+          className="absolute right-0 top-0 h-full md:h-[80%] xl:bg-[#E9EAEB]  xl:w-[40%] flex flex-col justify-center items-center gap-4 2xl:gap8 text-[#00b0db] xl:text-black/90"
           variants={descOpacity}
           animate={isInView ? 'open' : 'closed'}
         >
@@ -61,7 +64,7 @@ const ProgramLanding = () => {
             variants={descOpacity}
             animate={isInView ? 'open' : 'closed'}
             className={cn(
-              'text-sm 2xl:text-2xl w-[80%] 2xl:w-[50%] font-semibold m-0 ',
+              'text-xl md:text-sm 2xl:text-2xl w-[80%] 2xl:w-[50%] font-semibold m-0 ',
               isArabic ? 'arabic-subtitle-regular' : 'latin-subtitle-regular'
             )}
           >
@@ -71,7 +74,7 @@ const ProgramLanding = () => {
             variants={descOpacity}
             animate={isInView ? 'open' : 'closed'}
             className={cn(
-              'text-sm 2xl:text-2xl w-[80%] 2xl:w-[50%] font-semibold m-0 ',
+              'text-xl md:text-sm 2xl:text-2xl w-[80%] 2xl:w-[50%] font-semibold m-0 ',
               isArabic ? 'arabic-subtitle-regular' : 'latin-subtitle-regular'
             )}
           >
@@ -79,7 +82,7 @@ const ProgramLanding = () => {
           </motion.p>
         </motion.div>
         <motion.div
-          className="absolute left-0 -bottom-64 h-[80%] xl:bg-[#E9EAEB]  xl:w-[40%] flex flex-col justify-center items-center gap-4 2xl:gap8 text-[#00b0db] xl:text-black/90"
+          className="absolute left-0 -bottom-64 h-[80%] xl:bg-[#E9EAEB]  xl:w-[40%] flex-col justify-center items-start gap-4 2xl:gap8 text-[#00b0db] xl:text-black/90 hidden md:flex px-6"
           variants={descOpacity}
           animate={isInView ? 'open' : 'closed'}
         >
@@ -87,7 +90,7 @@ const ProgramLanding = () => {
             variants={descOpacity}
             animate={isInView ? 'open' : 'closed'}
             className={cn(
-              'text-sm 2xl:text-2xl w-[80%] 2xl:w-[50%] font-semibold m-0 ',
+              'text-xl md:text-sm 2xl:text-2xl w-[80%] 2xl:w-[50%] font-semibold m-0 ',
               isArabic ? 'arabic-subtitle-regular' : 'latin-subtitle-regular'
             )}
           >
@@ -97,13 +100,39 @@ const ProgramLanding = () => {
             variants={descOpacity}
             animate={isInView ? 'open' : 'closed'}
             className={cn(
-              'text-sm 2xl:text-2xl w-[80%] 2xl:w-[50%] font-semibold m-0 ',
+              'text-xl md:text-sm 2xl:text-2xl w-[80%] 2xl:w-[50%] font-semibold m-0 ',
               isArabic ? 'arabic-subtitle-regular' : 'latin-subtitle-regular'
             )}
           >
             {t2('Hero.subtitle3')}
           </motion.p>
+          <Link href={`#program_section`} className="hidden md:flex">
+            <RoundedBtn className='relative text-base group inline-flex items-center justify-center overflow-hidden rounded-full font-bold ring-offset-background transition-colors before:absolute before:left-[-10%] before:h-0 before:w-[120%] before:translate-y-3/4 before:scale-0 before:rounded-full before:pb-[120%] before:content-[""] after:absolute after:inset-0 after:h-full after:w-full after:-translate-y-full after:rounded-full after:transition-transform after:duration-300 after:ease-in-expo after:content-[""] hover:before:translate-y-0 hover:before:scale-100 hover:before:transition-transform hover:before:duration-300 hover:before:ease-in-expo hover:after:translate-y-0 hover:after:transition-transform hover:after:delay-300 hover:after:duration-75 hover:after:ease-linear focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:border-2 hover:border-solid  hover:border-[#094142] before:bg-[#094142] after:bg-[#094142] px-16 py-4 before:-top-1/2 hover:text-background'>
+              <p
+                className={cn(
+                  'relative z-[1] transition-colors duration-400 text-[#E9EAEB] md:text-[#094142] group-hover:text-[#00b0db] m-0 text-2xl md:text-4xl ',
+                  isArabic
+                    ? 'arabic-title-bold'
+                    : 'latin-title-bold  capitalize'
+                )}
+              >
+                {t('more')}
+              </p>
+            </RoundedBtn>
+          </Link>
         </motion.div>
+        <Link href={`#program_section`} className="absolute bottom-0 md:hidden">
+          <RoundedBtn className='relative text-base group inline-flex items-center justify-center overflow-hidden rounded-full font-bold ring-offset-background transition-colors before:absolute before:left-[-10%] before:h-0 before:w-[120%] before:translate-y-3/4 before:scale-0 before:rounded-full before:pb-[120%] before:content-[""] after:absolute after:inset-0 after:h-full after:w-full after:-translate-y-full after:rounded-full after:transition-transform after:duration-300 after:ease-in-expo after:content-[""] hover:before:translate-y-0 hover:before:scale-100 hover:before:transition-transform hover:before:duration-300 hover:before:ease-in-expo hover:after:translate-y-0 hover:after:transition-transform hover:after:delay-300 hover:after:duration-75 hover:after:ease-linear focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:border-2 hover:border-solid  hover:border-[#094142] before:bg-[#094142] after:bg-[#094142] px-16 py-4 before:-top-1/2 hover:text-background'>
+            <p
+              className={cn(
+                'relative z-[1] transition-colors duration-400 text-[#E9EAEB] md:text-[#094142] group-hover:text-[#00b0db] m-0 text-2xl md:text-4xl ',
+                isArabic ? 'arabic-title-bold' : 'latin-title-bold  capitalize'
+              )}
+            >
+              {t('more')}
+            </p>
+          </RoundedBtn>
+        </Link>
       </motion.div>
     </section>
   );
