fe5f314d608b80175f548022b33bea34f47417fa
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2024-11-19 12:38:39 +0300

    adjusting posts page layout

--- src/app/[locale]/derive-2024/page.tsx
+++ src/app/[locale]/derive-2024/page.tsx
@@ -5,6 +5,9 @@ import Contact from '../../../components/shared/contact';
 import SlidingImages from '@/components/shared/slider';
 import ProgramSection from '@/components/shared/ProgramsSection';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 const DerivePage = () => {
   const t = useTranslations('Layout');
   const pageName = t('Navigation.derive.title');
